3814c80163d254be6364264aee6777ff35274f1d
prajwol-ghimire <prajwolghimire19@gmail.com>
2024-06-08 16:56:10 +0545

    schema models

--- server/app/models/venueModel.js
+++ server/app/models/venueModel.js
@@ -1,5 +1,29 @@
 const mongoose = require("mongoose");
 
+const reviewSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",  // Assuming you have a User model defined
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    upvotes: {
+        type: Number,
+        default: 0,
+    },
+    downvotes: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
 const venueSchema = new mongoose.Schema({
     owner_id: {
         type: mongoose.Schema.Types.ObjectId,
@@ -14,10 +38,17 @@ const venueSchema = new mongoose.Schema({
         type: String,
         required: true,
     }],
+    amenities: [{
+        type: String,
+        required: true,
+    }],
+    minprice: {
+        type: Number,
+        required: true,
+    },
+    reviews: [reviewSchema]  // Array of reviews
 });
 
 const Venue = mongoose.model('Venue', venueSchema);
 
 module.exports = Venue;
-
- 

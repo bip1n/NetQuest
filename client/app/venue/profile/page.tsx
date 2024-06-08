d89eee4e85cad011bdfd390395f20652306958d7
bip1n <bhabishwar.201744@ncit.edu.np>
2024-06-09 00:05:16 +0545

    hero page

--- client/app/venue/profile/page.tsx
+++ client/app/venue/profile/page.tsx
@@ -31,32 +31,19 @@ export default function VenueProfile() {
     <>
       <Navigationbar />
       <div>
-        <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
-          <CardHeader className="absolute z-10 top-1 flex-col items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">Add to favourite</p>
-          </CardHeader>
-          <Image
-            removeWrapper
-            alt="Card example background"
-            className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-            src="https://previews.123rf.com/images/horimatsu/horimatsu1509/horimatsu150900030/45248004-futsal-court-in-a-public-outdoor-park-with-artificial-turf.jpg"
-          />
-          <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-            <div>
-            <p className="text-lg text-default uppercase font-bold">KICK FUTSAL</p>
-            </div>
-            <Chip
-              className="text-tiny"
-              color="secondary"
-              radius="sm"
-              size="sm"
-              variant="shadow"
-             
-            >
-              [3.4 / 5]
-            </Chip>
-          </CardFooter>
-      </Card>
+      <Card className="py-4">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start  justify-between">
+       <h4 className="font-semibold uppercase text-medium">Kick Futsal <span className="text-xs ml-2 text-blue-500"> [4.6/5]</span></h4>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        <Image
+         isZoomed
+          alt="Venue Image"
+          // className="object-cover rounded-xl"
+          src="https://previews.123rf.com/images/horimatsu/horimatsu1509/horimatsu150900030/45248004-futsal-court-in-a-public-outdoor-park-with-artificial-turf.jpg"
+        />
+      </CardBody>
+    </Card>
         
         {/* <Card className="w-full max-w-[100%] md:max-w-[600px] mt-2">
         <div className="flex w-full flex-col">
@@ -110,6 +97,7 @@ export default function VenueProfile() {
             }
           >
             <Reviews />
+            <Link href="/venue/reviews" className="ml-3 text-sm "> Read More...</Link>
           </Tab>
           
           <Tab

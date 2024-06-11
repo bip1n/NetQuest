f5e85abdcb9df2ff67dd11faf038fed7a11a52a0
bip1n <bhabishwar.201744@ncit.edu.np>
2024-06-11 15:07:22 +0545

    Add images in venue profile

--- client/app/venue/profile/setting/page.tsx
+++ client/app/venue/profile/setting/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React,{useState} from 'react'
 import { 
   Card,
   CardBody,
@@ -17,7 +17,17 @@ import { FooterContent } from '@/components/Footer'
 import {Time} from "@internationalized/date";
 import { ClockCircleLinearIcon } from '@/components/Icons'
 export default function ProfileSetting () {
-  return (
+  const [files, setFiles] = useState<File[]>([]);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = event.target.files;
+    if (fileList) {
+      // Limiting the number of files to 5
+      const selectedFiles = Array.from(fileList).slice(0, 5);
+      setFiles([...files, ...selectedFiles]);
+    }
+  };
+ return (
     <>
       <Navigationbar/>
       <Card>
@@ -25,16 +35,16 @@ export default function ProfileSetting () {
           <p className='text-secondary font-semibold'>Venue Details</p>
         </CardHeader>
         <CardBody>
-            <Input required  type='text' label="Futsal Username" placeholder='hello' />
+            <Input required  type='text' label="Futsal Username" defaultValue='Kick Futsal' />
         </CardBody>
         <CardBody>
-            <Input required  type='number' label="Contact" value='9876543210'/>
+            <Input required  type='number' label="Contact" defaultValue='9876543210'/>
         </CardBody>
         <CardBody>
-            <Input required  type='text' label="Location" value='Balkumari, Lalitpur'/>
+            <Input required  type='text' label="Location" defaultValue='Balkumari, Lalitpur'/>
         </CardBody>
         <CardBody>
-            <Input required  type='text' label="Maps Coordinate" value='asdkjeisaiugaiudgasd'/>
+            <Input required  type='text' label="Maps Coordinate" defaultValue='asdkjeisaiugaiudgasd'/>
         </CardBody>
         <CardBody>
         <div className="flex gap-4">
@@ -84,16 +94,32 @@ export default function ProfileSetting () {
       </Card>
 
       <Card className='mt-4'>
-        <CardHeader>
-          <p className='text-secondary font-semibold'>Upload Images</p>
-        </CardHeader>
-        
-        <CardFooter>
-          <Button color="secondary" variant="shadow">
-            Upload
-          </Button>
-        </CardFooter>
-      </Card>
+          <CardHeader>
+            <p className='text-secondary font-semibold'>Upload Images</p>
+          </CardHeader>
+          <CardBody>
+            <div className="flex flex-wrap">
+              <Input type="file" accept="image/*, video/*" multiple onChange={handleFileChange} className="w-full mb-4" />
+              {files.map((file, index) => (
+                <div key={index} className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-2">
+                  <CardBody className="bg-gray-100 p-2 rounded-lg">
+                    <p className='text-sm font-semibold'>{file.name}</p>
+                    {file.type.startsWith('image/') ? (
+                      <img src={URL.createObjectURL(file)} alt="Uploaded" className="max-w-full h-auto  max-h-50" />
+                    ) : (
+                      <video controls src={URL.createObjectURL(file)} className="max-w-full h-auto" />
+                    )}
+                  </CardBody>
+                </div>
+              ))}
+            </div>
+          </CardBody>
+          <CardFooter>
+            <Button color="secondary" variant="shadow">
+              Upload
+            </Button>
+          </CardFooter>
+        </Card>
       <FooterContent/>
     </>
   )

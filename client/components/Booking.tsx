ceeb3f069db54ab7090a85005551218fc982cf1f
bip1n <bhabishwar.201744@ncit.edu.np>
2024-06-11 11:43:58 +0545

    booking time

--- client/components/Booking.tsx
+++ client/components/Booking.tsx
@@ -25,9 +25,11 @@ import {
   ModalHeader,
   ModalBody,
   ModalFooter,
+  TimeInput,
 } from "@nextui-org/react";
-import { getLocalTimeZone, today, CalendarDate } from "@internationalized/date";
+import { getLocalTimeZone, today, CalendarDate,Time } from "@internationalized/date";
 import { useRouter } from 'next/navigation';
+import {ClockCircleLinearIcon} from './Icons';
 
 // Define a type for the slot objects
 interface Slot {
@@ -38,7 +40,7 @@ interface Slot {
 
 export const Booking = () => {
   const router = useRouter();
-  const venueOwner = false;
+  const venueOwner = true;
   const [isLoading, setIsLoading] = useState(true);
   const [slots, setSlots] = useState<Slot[]>([]);
   const [checkedSlots, setCheckedSlots] = useState<number[]>([]);
@@ -96,6 +98,30 @@ export const Booking = () => {
             />
           </div>
 
+          {venueOwner ? (
+            <>
+            <div className="mb-4">
+               <TimeInput 
+                  label="Opens At" 
+                  labelPlacement="inside" 
+                  defaultValue={new Time(6, 0)} 
+                  startContent={(
+                    <ClockCircleLinearIcon className="text-xl text-default-400 pointer-events-none flex-shrink-0" />
+                  )}
+                />
+            </div>
+            <div className="mb-4">
+              <TimeInput 
+                  label="Closes At" 
+                  labelPlacement="inside" 
+                  defaultValue={new Time(20, 0)} 
+                  startContent={(
+                    <ClockCircleLinearIcon className="text-xl text-default-400 pointer-events-none flex-shrink-0" />
+                  )}
+                />
+            </div>
+            </>
+            ) : (null)}
           {isLoading ? (
             <div className="flex justify-center items-center h-32">
               <Spinner label="Loading..." />
@@ -123,7 +149,9 @@ export const Booking = () => {
                             ? "success"
                             : slot.status === "BOOKED"
                             ? "danger"
-                            : "warning"
+                            :  slot.status === "RESERVED"
+                            ? "warning"
+                            : "default"
                         }
                       >
                         {slot.status}

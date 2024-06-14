e25b7e0b439b16d514ab379ac5517c09682d6d4f
bip1n <bhabishwar.201744@ncit.edu.np>
2024-06-14 20:25:45 +0545

    set rate 0 if status = unavailable

--- client/app/venue/booking-settings/page.tsx
+++ client/app/venue/booking-settings/page.tsx
@@ -15,7 +15,6 @@ export default function BookingSettingsPage() {
       <Card>
         <BookingTable/>
       </Card>
-     
       <FooterContent />
     </>
   );
--- client/components/BookingTable.tsx
+++ client/components/BookingTable.tsx
@@ -28,9 +28,10 @@ import {
   TimeInput,
   Input,
 } from "@nextui-org/react";
-import { getLocalTimeZone, today, CalendarDate,Time } from "@internationalized/date";
+import { getLocalTimeZone, today, CalendarDate, Time } from "@internationalized/date";
 import { useRouter } from 'next/navigation';
-import {ClockCircleLinearIcon} from './Icons';
+import { ClockCircleLinearIcon } from './Icons';
+
 // Define a type for the slot objects
 interface Slot {
   time: string;
@@ -65,6 +66,9 @@ export const BookingTable = () => {
   const handleStatusChange = (slotIndex: number, newStatus: string) => {
     const updatedSlots = [...slots];
     updatedSlots[slotIndex].status = newStatus;
+    if (newStatus === "UNAVAILABLE") {
+      updatedSlots[slotIndex].rate = 0;
+    }
     setSlots(updatedSlots);
   };
 
@@ -85,9 +89,9 @@ export const BookingTable = () => {
     <>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <CardBody>
-        <CardHeader><h4 className="font-medium text-lg text-secondary">Open Hours and Rate</h4></CardHeader>
+          <CardHeader><h4 className="font-medium text-lg text-secondary">Open Hours and Rate</h4></CardHeader>
           <CardBody>
-          <div className="mb-4">
+            <div className="mb-4">
               <DatePicker
                 label={"Select Date"}
                 className="max-w-[284px]"
@@ -99,151 +103,101 @@ export const BookingTable = () => {
                 onChange={(date) => setSelectedDate(date)}
               />
             </div>
-            
-          <div className="flex gap-4">
-            <TimeInput
-              label="Opens At"
-              labelPlacement="inside"
-              defaultValue={new Time(6, 0)}
-            //  value={new Time(11, 45)}
-            //   onChange={setOpensAt}
-              startContent={
-                <ClockCircleLinearIcon className="text-xl text-default-400 pointer-events-none flex-shrink-0" />
-              }
-            />
-            <TimeInput
-              label="Closes At"
-              labelPlacement="inside"
-              defaultValue={new Time(19, 0)}
-            //   value={closesAt}
-            //   onChange={setClosesAt}
-              startContent={
-                <ClockCircleLinearIcon className="text-xl text-default-400 pointer-events-none flex-shrink-0" />
-              }
-            />
-          </div>
-           </CardBody>
-           <CardBody>
-           <Input
+
+            <div className="flex gap-4">
+              <TimeInput
+                label="Opens At"
+                labelPlacement="inside"
+                defaultValue={new Time(6, 0)}
+                startContent={
+                  <ClockCircleLinearIcon className="text-xl text-default-400 pointer-events-none flex-shrink-0" />
+                }
+              />
+              <TimeInput
+                label="Closes At"
+                labelPlacement="inside"
+                defaultValue={new Time(19, 0)}
+                startContent={
+                  <ClockCircleLinearIcon className="text-xl text-default-400 pointer-events-none flex-shrink-0" />
+                }
+              />
+            </div>
+          </CardBody>
+          <CardBody>
+            <Input
               type="email"
               label="Rate"
               labelPlacement="inside"
               defaultValue="1200"
             />
             <Checkbox defaultSelected isReadOnly className="mt-2"> Apply Same For All</Checkbox>
-           </CardBody>
+          </CardBody>
 
-           <CardFooter> <Button color="primary" variant="solid"> Save Changes</Button></CardFooter>
-           <CardHeader><h4 className="font-medium text-lg text-secondary">Booking Status</h4></CardHeader>
-            <Table  aria-label="Example static collection table">
-              <TableHeader>
-                <TableColumn>TIME</TableColumn>
-                <TableColumn>RATE</TableColumn>
-                <TableColumn>STATUS</TableColumn>
-                <TableColumn>{venueOwner ? "EDIT" : "SELECT"}</TableColumn>
-              </TableHeader>
+          <CardFooter> <Button color="primary" variant="solid"> Save Changes</Button></CardFooter>
+          <CardHeader><h4 className="font-medium text-lg text-secondary">Booking Status</h4></CardHeader>
+          <Table aria-label="Example static collection table">
+            <TableHeader>
+              <TableColumn>TIME</TableColumn>
+              <TableColumn>RATE</TableColumn>
+              <TableColumn>STATUS</TableColumn>
+              <TableColumn>{venueOwner ? "EDIT" : "SELECT"}</TableColumn>
+            </TableHeader>
 
-              <TableBody>
-                {slots.map((slot, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{slot.time}</TableCell>
-                    <TableCell>{slot.rate}</TableCell>
-                    <TableCell>
-                      <Chip
-                        radius="sm"
-                        size="sm"
-                        color={
-                          slot.status === "AVAILABLE"
-                            ? "success"
-                            : slot.status === "BOOKED"
+            <TableBody>
+              {slots.map((slot, index) => (
+                <TableRow key={index}>
+                  <TableCell>{slot.time}</TableCell>
+                  <TableCell>{slot.rate}</TableCell>
+                  <TableCell>
+                    <Chip
+                      radius="sm"
+                      size="sm"
+                      color={
+                        slot.status === "AVAILABLE"
+                          ? "success"
+                          : slot.status === "BOOKED"
                             ? "danger"
-                            :  slot.status === "RESERVED"
-                            ? "warning"
-                            : "default"
-                        }
+                            : slot.status === "RESERVED"
+                              ? "warning"
+                              : "default"
+                      }
+                    >
+                      {slot.status}
+                    </Chip>
+                  </TableCell>
+                  <TableCell>
+                    <Dropdown backdrop="opaque">
+                      <DropdownTrigger>
+                        <Button size="sm" variant="shadow" color="secondary">
+                          EDIT
+                        </Button>
+                      </DropdownTrigger>
+                      <DropdownMenu
+                        variant="faded"
+                        onAction={(key) => handleStatusChange(index, key as string)}
                       >
-                        {slot.status}
-                      </Chip>
-                    </TableCell>
-                    <TableCell>
-                        <Dropdown backdrop="opaque">
-                          <DropdownTrigger>
-                            <Button size="sm" variant="shadow" color="secondary">
-                              EDIT
-                            </Button>
-                          </DropdownTrigger>
-                          <DropdownMenu
-                            variant="faded"
-                            onAction={(key) => handleStatusChange(index, key as string)}
-                          >
-                            <DropdownItem key="AVAILABLE">
-                              <p className="text-green-500">AVAILABLE</p>
-                            </DropdownItem>
-                            <DropdownItem key="RESERVED">
-                              <p className="text-orange-400">RESERVED</p>
-                            </DropdownItem>
-                            <DropdownItem key="BOOKED">
-                              <p className="text-red-600">BOOKED</p>
-                            </DropdownItem>
-                            <DropdownItem key="UNAVAILABLE">
-                              <p>UNAVAILABLE</p>
-                            </DropdownItem>
-                          </DropdownMenu>
-                        </Dropdown>
-                     
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          <CardFooter>
-            <p className="italic text-xs">
-              <span className="text-red-500"> * </span>The{" "}
-              <span className="text-orange-500">RESERVED</span> slot/s might change
-              later.<span className="text-red-500"> *</span>
-            </p>
-          </CardFooter>
+                        <DropdownItem key="AVAILABLE">
+                          <p className="text-green-500">AVAILABLE</p>
+                        </DropdownItem>
+                        <DropdownItem key="RESERVED">
+                          <p className="text-orange-400">RESERVED</p>
+                        </DropdownItem>
+                        <DropdownItem key="BOOKED">
+                          <p className="text-red-600">BOOKED</p>
+                        </DropdownItem>
+                        <DropdownItem key="UNAVAILABLE">
+                          <p>UNAVAILABLE</p>
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </CardBody>
       </div>
-      <div className="flex flex-col gap-2">
-        <Modal
-          isOpen={isOpen}
-          placement="bottom-center"
-          onOpenChange={setIsOpen}
-        >
-          <ModalContent>
-            {(onClose) => (
-              <>
-                <ModalHeader className="flex flex-col gap-1 text-secondary">
-                  Selected Shift/s
-                </ModalHeader>
-                <ModalBody>
-                  {selectedSlots.map((slot, index) => (
-                    <p key={index}>
-                      Date: {selectedDate.toString()}
-                      <br />
-                      Time: {slot.time}
-                      <br />
-                      Rate: Rs. {slot.rate}
-                    </p>
-                  ))}
-                  <p>Total: Rs. {totalRate}</p>
-                </ModalBody>
-                <ModalFooter>
-                  <Button color="danger" variant="light" onPress={onClose}>
-                    Cancel
-                  </Button>
-                  <Button color="secondary" variant="shadow"  onClick={() => {
-                      router.push("/venue/booking/checkout");
-                    }}>
-                    Book
-                  </Button>
-                </ModalFooter>
-              </>
-            )}
-          </ModalContent>
-        </Modal>
-      </div>
+      
     </>
   );
 };

12702026043dce64758fbdbe2082d8ba11c9e1cb
bip1n <bhabishwar.201744@ncit.edu.np>
2024-07-27 10:38:51 +0545

    test

--- client/app/(venue)/venue/history/page.tsx
+++ client/app/(venue)/venue/history/page.tsx
@@ -8,33 +8,32 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 
 const BookingHistory = () => {
   return (
     <main className="flex flex-col justify-center p-4">
       <Table>
-  <TableCaption>A list of your recent invoices.</TableCaption>
-  <TableHeader>
-    <TableRow>
-      <TableHead className="w-[100px]">DATE</TableHead>
-      <TableHead>TIME</TableHead>
-      <TableHead>USER</TableHead>
-      <TableHead>STATUS</TableHead>
-      <TableHead className="text-right">AMOUNT</TableHead>
-    </TableRow>
-  </TableHeader>
-  <TableBody>
-    <TableRow>
-      <TableCell className="font-medium">INV001</TableCell>
-      <TableCell>Paid</TableCell>
-      <TableCell>Credit Card</TableCell>
-      <TableCell>Credit Card</TableCell>
-      <TableCell className="text-right">$250.00</TableCell>
-    </TableRow>
-  </TableBody>
-</Table>
-
+        <TableCaption>Booking History</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">DATE</TableHead>
+            <TableHead>TIME</TableHead>
+            <TableHead>USER</TableHead>
+            <TableHead>STATUS</TableHead>
+            <TableHead className="text-right">AMOUNT</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </main>
   );
 };
